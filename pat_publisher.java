5295c23d13e81bd817ecb50d5b46355a8bde1a90
claudioballardini01 <38256821+claudioballardini01@users.noreply.github.com>
2026-03-26 17:05:03 +0100

    Update and rename publisher.java to pat_publisher.java

---
+++ pat_publisher.java
@@ -0,0 +1,87 @@
+{
+  "id": "structureP330",
+  "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/strutture-organizzative/structureP330#Umst-digitalizzazione-e-reti",
+  "published_at": "2025-03-25T09:58:51+01:00",
+  "modified_at": "2026-03-26T04:01:41+01:00",
+  "is_public": true,
+  "alt_name": "",
+  "abstract": "",
+  "type": [
+    "UMSt - Unità di missione strategica"
+  ],
+  "logo_image": [],
+  "topics": [
+    {
+      "id": "fb69024235f7382d500ac37841d5d00c",
+      "uri": "https://www.provincia.tn.it/api/openapi/argomenti/fb69024235f7382d500ac37841d5d00c#Trasparenza",
+      "priority": 1
+    }
+  ],
+  "image": [],
+  "main_function": "<p class=\" text-justify\">L’Unità di missione strategica  digitalizzazione e reti:</p>\n<ul>\n\n<li>in accordo con le linee di indirizzo nazionali ed europee, cura il coordinamento delle politiche di utilizzo delle ICT quale strumento per migliorare la relazione con cittadini e imprese</li>\n\n<li>assicura il coordinamento delle iniziative strategiche in materia di amministrazione digitale, dematerializzazione dei processi e dei documenti e di transizione al digitale</li>\n\n<li>coordina le attività legate all’utilizzo e alla diffusione delle tecnologie</li>\n\n<li>coordina le attività della Provincia in materia di open data</li>\n\n<li>coordina le attività relative al sistema dei portali provinciali</li>\n\n<li>sovrintende l’attuazione delle misure del PNRR di interesse del territorio per gli aspetti di competenza e coordina quelle concernenti gli interventi trasversali per la transizione digitale</li>\n\n<li>cura, sulla base delle indicazioni fornite dalla Direzione generale, le attività di coordinamento sui temi dello sviluppo della connettività e delle reti</li>\n\n<li>sviluppa la diffusione della banda ultralarga nel Trentino, svolgendo l’attività di coordinamento, gestione ed integrazione dei differenti sistemi di connettività avanzata presenti sul territorio e relative infrastrutture</li>\n\n<li>cura, con il Dipartimento protezione civile, l’attività di coordinamento delle diverse reti di emergenza e di supporto alla protezione civile avvalendosi della società di sistema incaricata</li>\n\n<li>cura i rapporti con i soggetti pubblici e privati operanti nel settore delle telecomunicazioni, ivi compresi i ministeri competenti e AGCOM, anche in relazione agli aspetti autorizzativi relativi alle reti private della Provincia. Detta attività si esplicherà altresì nel coordinare gli operatori della telefonia mobile affinché provvedano alla copertura del segnale radio nelle gallerie</li>\n\n</ul>\n",
+  "has_spatial_coverage": [
+    {
+      "id": "placeP330",
+      "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/luoghi/placeP330#Sede-di-Umst-digitalizzazione-e-reti",
+      "priority": 1
+    }
+  ],
+  "has_online_contact_point": [
+    {
+      "id": "contactpointP330",
+      "uri": "https://www.provincia.tn.it/api/openapi/media/classificazioni/punti-di-contatto/contactpointP330#Contatti-di-Umst-digitalizzazione-e-reti",
+      "priority": 1
+    }
+  ],
+  "has_spatial_coverage_other": [],
+  "has_online_contact_point_other": [],
+  "link": [],
+  "is_support_unit_of": [
+    {
+      "id": "structureP001",
+      "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/strutture-organizzative/structureP001#Direzione-generale-della-Provincia",
+      "priority": 1
+    }
+  ],
+  "has_attachment": [
+    {
+      "id": "delibera_558-2019",
+      "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/documenti/delibera_558-2019#Deliberazione-n.-558-del-19-04-2019",
+      "priority": 1
+    },
+    {
+      "id": "delibera_425-2019",
+      "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/documenti/delibera_425-2019#Deliberazione-n.-425-del-25-03-2019",
+      "priority": 2
+    },
+    {
+      "id": "delibera_2462-2018",
+      "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/documenti/delibera_2462-2018#Deliberazione-n.-2462-del-21-12-2018",
+      "priority": 3
+    },
+    {
+      "id": "delibera_257-2020",
+      "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/documenti/delibera_257-2020#Deliberazione-n.-257-del-21-02-2020",
+      "priority": 4
+    },
+    {
+      "id": "delibera_694-2022",
+      "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/documenti/delibera_694-2022#Deliberazione-n.-694-del-22-04-2022",
+      "priority": 5
+    },
+    {
+      "id": "delibera_292-2024",
+      "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/documenti/delibera_292-2024#Deliberazione-n.-292-del-08-03-2024",
+      "priority": 6
+    },
+    {
+      "id": "delibera_157-2024",
+      "uri": "https://www.provincia.tn.it/api/openapi/amministrazione/documenti/delibera_157-2024#Deliberazione-n.-157-del-09-02-2024",
+      "priority": 7
+    }
+  ],
+  "more_information": "<p><a href=\"/Amministrazione/Documenti/Informativa-generale-privacy-Umst-digitalizzazione-e-reti#page-content\" target=\"_blank\">Informativa generale privacy</a></p>",
+  "identifier": "P330",
+  "ipa_code": "SN1W1H",
+  "resource_name": "TypedOrganization"
+}
